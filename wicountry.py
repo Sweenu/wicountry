e271bb65674b70253044874614497bde9011a0cc
Bruno Inec <brunoinec@gmail.com>
2018-09-19 09:51:25 +0200

    kinda working example

--- wicountry.py
+++ wicountry.py
@@ -1,9 +1,19 @@
+import json
+from pprint import pprint
+
 import click
+
+
+with open('data.json', 'r') as f:
+    data = json.load(f)
 
 @click.command()
 @click.argument('country')
-def main(country):
-    pass
+@click.option('--themes', '-t',
+              help='A comma separated list of themes you want to see')
+def main(country, themes):
+    info = data.get(country)
+    pprint(info)
 
 
 if __name__ == '__main__':
